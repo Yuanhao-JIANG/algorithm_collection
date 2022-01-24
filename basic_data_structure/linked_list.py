072d1c8dee6f21ddbf357b40e9527026b163163b
Yuanhao-JIANG <71644411+Yuanhao-JIANG@users.noreply.github.com>
2022-01-24 11:46:26 +0000

    implement linked list and graph data structure

---
+++ basic_data_structure/linked_list.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+
+class LinkedList:
+    def __init__(self, head_node=None):
+        self.head_node = head_node
+
+    def last_node(self):
+        curr_node = self.head_node
+        while curr_node.next_node is not None:
+            curr_node = curr_node.next_node
+        return curr_node
+
+    def has_data(self, i):
+        curr_node = self.head_node
+        while curr_node is not None:
+            if curr_node.data == i:
+                return True
+            curr_node = curr_node.next_node
+        return False
+
+    def list_all(self):
+        result = []
+        curr_node = self.head_node
+        while curr_node is not None:
+            result.append(curr_node.data)
+        return result
---
+++ graph/graph.py
@@ -0,0 +1,57 @@
+import numpy as np
+from basic_data_structure.linked_list import LinkedList, Node
+
+
+class AdjacencyMatrix:
+    def __init__(self, n):
+        self.vertex_size = n
+        self.mat = np.zeros((n, n))
+
+    def is_edg(self, i, j):
+        if self.mat[i][j]:
+            return True
+        else:
+            return False
+
+    def set_edg(self, i, j):
+        self.mat[i][j] = 1
+
+    def out(self, i):
+        out_list = []
+        for j in range(self.vertex_size):
+            if self.mat[i][j] == 1:
+                out_list.append(j)
+        return out_list
+
+    def in_(self, j):
+        in_list = []
+        for i in range(self.vertex_size):
+            if self.mat[i][j] == 1:
+                in_list.append(i)
+        return in_list
+
+
+class AdjacencyList:
+    def __init__(self, n):
+        self.vertex_size = n
+        self.vertex_list = []
+        for i in range(n):
+            linked_list = LinkedList()
+            self.vertex_list.append(linked_list)
+
+    def is_edg(self, i, j):
+        return self.vertex_list[i].has_data(j)
+
+    def set_edg(self, i, j):
+        new_node = Node(j)
+        self.vertex_list[i].last_node().next_node = new_node
+
+    def out(self, i):
+        return self.vertex_list[i].list_all()
+
+    def in_(self, j):
+        in_list = []
+        for i in self.vertex_size:
+            if self.vertex_list[i].has_data(j):
+                in_list.append(i)
+        return in_list
