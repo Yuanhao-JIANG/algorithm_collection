3574174685b9e043b8b7008f87091b63936fd818
Yuanhao-JIANG <71644411+Yuanhao-JIANG@users.noreply.github.com>
2022-01-29 16:57:22 +0000

    refactor

--- basic_data_structure/linked_list.py
+++ basic_data_structure/linked_list.py
@@ -1,9 +1,3 @@
-class Node:
-    def __init__(self, data=None, next_node=None):
-        self.data = data
-        self.next_node = next_node
-
-
 class LinkedList:
     def __init__(self, head_node=None):
         self.head_node = head_node
